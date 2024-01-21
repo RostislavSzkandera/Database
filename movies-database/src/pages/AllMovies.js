5df2208406e061b9d9e5707603c64189f8d38351
Rostislav Szkandera <szkandera.r@seznam.cz>
2024-01-21 10:33:38 +0100

    Dokončená stránka AllMovies

--- movies-database/src/pages/AllMovies.js
+++ movies-database/src/pages/AllMovies.js
@@ -1,10 +1,49 @@
+import { projectFirestore } from "../firebase/config"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 
 
 const AllMovies = () => {
+    const [data, setData] = useState([])
+    const [error, setError] = useState(false)
+  
+  
+    useEffect( ()=> {
+      const unsubscribe = projectFirestore.collection("moviesapp").onSnapshot( (snapshot) => {
+        if(snapshot.empty) {
+          setError("V databázi není žádný film, musíte nějaký přidat")
+          setData([])
+        } else {
+          let result = []
+          snapshot.docs.forEach( (oneMovie) => {
+            result.push( {id: oneMovie.id, ...oneMovie.data()})
+          })
+          setData(result)
+        }
+      },(err) => setError(err.message) )
+      return () => unsubscribe()
+    }, [])
+
+    const deleteMovie = (id) => {
+      projectFirestore.collection("moviesapp").doc(id).delete()
+    }
+
   return (
-    <div>
-      AllMovies
-    </div>
+    <section className="w-9/12 flex flex-col mt-8">
+      {error ? <p className="text-2xl">{error}</p>: <h2 className="text-2xl mb-8 text-center ">Seznam filmů</h2>}
+      {
+        data.map( (oneMovie) => {
+          const { id, title } = oneMovie
+          return <div className="bg-primary flex flex-row justify-between items-center  mb-2 p-4 border border-primary" key={id}>
+              <h2 className="w-1/3 sm:w-1/2 border border-primary">{title}</h2>
+              <div className="flex flex-col sm:flex-row justify-center items-center">
+                            <Link to={`/one-movie/${id}`}>Více informací</Link>
+                            <button className="bg-button rounded ml-4 p-1 hover:bg-hover" onClick={ () => deleteMovie(id)}>Smazat</button>
+              </div>
+          </div>
+        })
+      }
+    </section>
   )
 }
 
--- movies-database/tailwind.config.js
+++ movies-database/tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     colors: {
       primary: "black",
       gray: "#0f5668",
-      white: "#e5e5e5"
+      white: "#e5e5e5",
+      button: "#dc2626",
+      hover: "#b91c1c"
     },
     extend: {
       
